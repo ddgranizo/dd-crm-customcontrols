18da48e3d4fb7f0a8202c10213c2faa4a0cbb98a
Daniel Diaz Granizo <daniel.diazg@scmspain.com>
2019-08-25 08:48:23 +0200

    Updates

--- precache-manifest.79c16b8ce9a8f58cbec6c6dd22aca7de.js
+++ precache-manifest.79c16b8ce9a8f58cbec6c6dd22aca7de.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "09bcf863241eae5025ac40741e1034e3",
+    "revision": "47b022937a8b830268f49fffa87f4cc1",
     "url": "/dd-crm-customcontrols/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dd-crm-customcontrols/static/css/2.62908225.chunk.css"
   },
   {
-    "revision": "659c8d4f03a567125570",
+    "revision": "b0e59390a7a11ae9e02c",
     "url": "/dd-crm-customcontrols/static/css/main.17c12e5f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dd-crm-customcontrols/static/js/2.3b8326bd.chunk.js"
   },
   {
-    "revision": "659c8d4f03a567125570",
-    "url": "/dd-crm-customcontrols/static/js/main.39cb866a.chunk.js"
+    "revision": "b0e59390a7a11ae9e02c",
+    "url": "/dd-crm-customcontrols/static/js/main.fc363e77.chunk.js"
   },
   {
     "revision": "b908286c8572275c1c30",
