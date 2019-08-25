f3d0a4038829dd1d8e58275054db43dc05b1410c
Daniel Diaz Granizo <daniel.diazg@scmspain.com>
2019-08-25 08:42:48 +0200

    Updates

---
+++ precache-manifest.634863b09eaea5e30122cb6e68adc287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e85d57fbb61d3c052451692d256767",
+    "url": "/dd-crm-customcontrols/index.html"
+  },
+  {
+    "revision": "a2cc567498972971780c",
+    "url": "/dd-crm-customcontrols/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "f5c0e19126d977fa2fee",
+    "url": "/dd-crm-customcontrols/static/css/main.17c12e5f.chunk.css"
+  },
+  {
+    "revision": "a2cc567498972971780c",
+    "url": "/dd-crm-customcontrols/static/js/2.3b8326bd.chunk.js"
+  },
+  {
+    "revision": "f5c0e19126d977fa2fee",
+    "url": "/dd-crm-customcontrols/static/js/main.3afcb07d.chunk.js"
+  },
+  {
+    "revision": "b908286c8572275c1c30",
+    "url": "/dd-crm-customcontrols/static/js/runtime~main.ef7b5753.js"
+  }
+]);
